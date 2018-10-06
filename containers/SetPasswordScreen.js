fc31ff470328e5c8fd886967339a9b072767bfba
MacBook <chen@MacBook-Air.local>
2018-10-06 19:45:06 +0800

    update create account

--- containers/SetPasswordScreen.js
+++ containers/SetPasswordScreen.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import {Alert, AsyncStorage, View, TextInput, Text, Button} from 'react-native'
 
+var bip39 = require('bip39')
+var HDKey = require('hdkey')
+const ethUtil = require('ethereumjs-util')
+
 export default class SetPasswordScreen extends Component {
   static navigationOptions = {
     title: '创建钱包'
@@ -27,18 +31,33 @@ export default class SetPasswordScreen extends Component {
   }
 
   _createWallet = () => {
-    this.setState({fetching:true})
-    console.log(this.state.password)
-    web3.eth.personal.newAccount(this.state.password).then((account)=>{
-      console.log(account)
-      this._storeAccount(account)
-      this.props.navigation.navigate('App')
-    }).catch((error)=>Alert.alert(error.toString()))
+    //this.setState({fetching:true})
+    //console.log(this.state.password)
+    //web3.eth.personal.newAccount(this.state.password).then((account)=>{
+    //  console.log(account)
+    //  this._storeAccount(account)
+    //  this.props.navigation.navigate('App')
+    //}).catch((error)=>Alert.alert(error.toString()))
+    const mnemonic = bip39.generateMnemonic()
+    console.log(mnemonic)
+    const seed = bip39.mnemonicToSeed(mnemonic)
+    const root = HDKey.fromMasterSeed(seed)
+    const masterPrivateKey = root.privateKey.toString('hex')
+    console.log(masterPrivateKey)
+    const addrNode = root.derive("m/44'/60'/0'/0/0")
+    const pubKey = ethUtil.privateToPublic(addrNode._privateKey);
+    console.log(pubKey)
+    const addr = ethUtil.publicToAddress(pubKey).toString('hex')
+    console.log(addr)
+    const address = ethUtil.toChecksumAddress(addr);
+    console.log(address)
+    AsyncStorage.multiSet([['mnemonic', mnemonic], ['account', address], ['password', this.state.password]])
+    this.props.navigation.navigate('App')
   }
 
-  _storeAccount = async(account) => {
-    await AsyncStorage.setItem('account', account)
-  }
+  //_storeAccount = async(account) => {
+  //  await AsyncStorage.setItem('account', account)
+  //}
 
   _renderButton = () => {
     if(!this.state.passwordValid) {
