8aeed5c4f6144bf60e0df24c76c920a88f931204
MacBook <chen@MacBook-Air.local>
2018-10-14 23:15:47 +0800

    add status, update wallet tokens, prices, balances for WalletScreen

--- containers/WalletScreen.js
+++ containers/WalletScreen.js
@@ -4,7 +4,8 @@ import Icon from 'react-native-vector-icons/Feather'
 import {formatBalance} from '../utils'
 import tokens from '../tokens'
 
-//global.web3.eth.getAccounts().then(console.log)
+global.web3.eth.getAccounts().then(console.log)
+console.log(web3.currentProvider.connected)
 
 const {width} = Dimensions.get('window')
 
@@ -17,59 +18,67 @@ export default class WalletScreen extends Component {
     super(props)
     this.state = {
       account: null,
-      balance: '0.0000',
-      value: 0.00,
-      price: 0,
-      coins: [],
-      coins_price: [],
-      refreshing: false
+      refreshing: false,
+      coins: [], // 用户收藏token
+      balances: [], // token数量列表
+      prices: [], // 市场价格列表
     }
   }
 
   componentDidMount() {
     DeviceEventEmitter.addListener('mycoins_changed', (e)=>this._getCoins())
-    this._getCoins()
     this._getAccount()
-    this._getPrice()
-    this._getTokenBalance()
   }
 
+  // 获取用户收藏的token列表
   _getCoins = async() => {
     const coins = await AsyncStorage.getItem('mycoins')
     console.log(coins)
     this.setState({coins:JSON.parse(coins)})
-    this._getPrice()
+    this._getTokenBalance() // 获取每个token数量
+    this._getPrice() // 获取每个token价格
   }
 
+  // 获取用户token的市场价格
   _getPrice = async() => {
-    const prices = await AsyncStorage.getItem('coins_price')
+    const prices = await AsyncStorage.getItem('prices')
+    console.log(prices)
     if(prices) {
-      this.setState({coins_price: JSON.parse(prices)})
+      this.setState({prices: JSON.parse(prices)})
     } else {
       this._fetchPrice()
     }
-    console.log(this.state.coins_price)
+    //console.log(this.state.prices.BAT.quote.CNY.price)
   }
 
-  /** 获取所有token金额 **/
+  // 获取用户所有token数量
   _getTokenBalance = () => {
     this.state.coins.map((item) => {
-      console.log('b+', item)
-      if(item !== 'ETH') {
-        /** 获取单个token金额 **/
+      if(item == 'ETH') {
+        this._getEthBalance()
+      } else {
+        // 获取单个token数量
         this._getTokenBalanceItem(item)
       }
     })
   }
 
-  /** 获取单个token金额 **/
+  // 获取单个token数量
   _getTokenBalanceItem = (token) => {
     console.log('b+', token)
     let tokenData = tokens[token]
+    if(!tokenData) {
+      return
+    }
+    console.log(tokenData)
     let contract = new web3.eth.Contract(JSON.parse(tokenData.abi), tokenData.address)
-    contract.methods.balanceOf(this.state.account).call().then((res) => console.log('b+', res))
+    console.log(this.state.account)
+    //console.log(Object.keys(contract.methods).toString())
+    console.log(contract.methods.balanceOf(this.state.account).call())
+    contract.methods.balanceOf(this.state.account).call().then((res) => console.log('b+', res)).catch((error)=>console.log(error))
   }
 
+  // 从接口获取tokens的市场价格
   _fetchPrice = () => {
     let coins = this.state.coins.toString()
     let url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${coins}&convert=CNY`
@@ -84,8 +93,8 @@ export default class WalletScreen extends Component {
       .then((response) => response.json())
       .then((responseJson) => {
         console.log(responseJson.data)
-        AsyncStorage.setItem('coins_price', JSON.stringify(responseJson.data))
-        this.setState({coins_price: responseJson.data})
+        AsyncStorage.setItem('prices', JSON.stringify(responseJson.data))
+        this.setState({prices: responseJson.data})
       })
       .catch((error)=>console.log('api error'))
 
@@ -94,79 +103,114 @@ export default class WalletScreen extends Component {
   _onRefresh = () => {
     this.setState({refreshing:true})
     //this._getPrice()
-    this._getBalance().then(()=>this.setState({refreshing:false}))
+    //this._getBalance().then(()=>this.setState({refreshing:false}))
 
   }
 
   _getAccount = async() => {
     const account = await AsyncStorage.getItem('account')
     this.setState({account:account})
-    this._getBalance()
+    this._getCoins()
+    //this._getBalance()
   }
 
-  _getBalance = () => {
+  // 获取eth数量
+  _getEthBalance = () => {
     return web3.eth.getBalance(this.state.account)
       .then((res)=>web3.utils.fromWei(res, 'ether'))
-      .then((balance)=>this.setState({balance:balance}))
+      .then((balance)=>{
+        let balances = this.state.balances
+        balances['ETH'] = balance
+        this.setState({balances:balances})
+      })
       .catch((error)=>Alert.alert(error.toString()))
   }
 
   _renderHeader = () => (
     <View style={styles.header}>
-      <Text style={{color:'white'}}>币加</Text>
+      <View style={{flexDirection:'row'}}>
+        <Text style={{color:'white',marginRight:10}}>币加</Text>
+        {web3.currentProvider.connected ? (
+          <Text style={{color:'green'}}>正常</Text>
+        ) : (
+          <Text style={{color:'red'}}>未连接</Text>
+        )}
+      </View>
       <Icon name='maximize' size={20} color='white' />
     </View>
   )
 
-  _renderMain = () => (
-    <ImageBackground source={require('../images/log-bg.png')} style={styles.main}>
-      <View style={{paddingHorizontal:20,paddingVertical:30}}>
-        <Text style={{fontWeight:'600',fontSize:12,color:'#4a4a4a',marginBottom:10}}>
-          我的资产</Text>
-        <View style={{flexDirection:'row',alignItems:'center'}}>
-          <Text style={{marginRight:10,backgroundColor:'#ff9a00',borderRadius:2,paddingVertical:2,paddingHorizontal:6,fontSize:12,color:'white'}}>CNY</Text>
-          <Text style={{fontSize:28,color:'rgb(81,81,114)'}}>{formatBalance(this.state.balance, 4)}</Text>
+  _renderMain = () => {
+    // 计算总资产
+    let balance = 0.0000
+    this.state.balances.map(item => {
+      let priceObj = this.state.prices[item]
+      let price = priceObj ? priceObj.quote.CNY.price : 0.00
+      balance += item * price
+    })
+    console.log(balance)
+    return (
+      <ImageBackground
+        source={require('../images/log-bg.png')} style={styles.main}>
+        <View style={{paddingHorizontal:20,paddingVertical:30}}>
+          <Text style={{fontWeight:'600',fontSize:12,color:'#4a4a4a',marginBottom:10}}>
+            我的资产</Text>
+          <View style={{flexDirection:'row',alignItems:'center'}}>
+            <Text style={{marginRight:10,backgroundColor:'#ff9a00',borderRadius:2,paddingVertical:2,paddingHorizontal:6,fontSize:12,color:'white'}}>CNY</Text>
+            <Text style={{fontSize:28,color:'rgb(81,81,114)'}}>{balance.toFixed(4)}</Text>
+          </View>
         </View>
-      </View>
-      <View style={{
-        backgroundColor: 'rgb(232,236,245)',
-        paddingVertical: 5,
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius: 5,
-        flexDirection: 'row',
-      }}>
-      <TouchableOpacity
-        style={[styles.mainAction, {borderRightWidth:1}]}
-        onPress={()=>this.props.navigation.navigate('Password',{to:'Transfer'})}>
-          <Image
-            source={require('../images/wallet_send.png')}
-            style={{width:18,height:18,marginRight:10}} />
-          <Text>转 账</Text>
-        </TouchableOpacity>
+        <View style={{
+          backgroundColor: 'rgb(232,236,245)',
+          paddingVertical: 5,
+          borderBottomLeftRadius: 5,
+          borderBottomRightRadius: 5,
+          flexDirection: 'row',
+        }}>
         <TouchableOpacity
-          style={styles.mainAction}
-          onPress={()=>this.props.navigation.navigate('Receipt')}>
-          <Image
-            source={require('../images/wallet_receive.png')}
-            style={{width:18,height:18,marginRight:10}} />
-          <Text>收 款</Text>
-        </TouchableOpacity>
-      </View>
-    </ImageBackground>
-  )
-
-  _renderItem = ({item}) => (
-    <TouchableOpacity onPress={()=>this.props.navigation.navigate('Log')}>
-      <View style={styles.item}>
-        <Text style={{color:'rgb(78,78,78)'}}>{item.toString()}</Text>
-        <View style={{alignItems:'flex-end'}}>
-          <Text style={{color:'rgb(46,46,46)'}}>{this.state.balance}</Text>
-          <Text style={{fontSize:12,color:'rgb(184,186,206)'}}>
-            ≈{formatBalance(this.state.balance*this.state.price, 2)}</Text>
+          style={[styles.mainAction, {borderRightWidth:1}]}
+          onPress={()=>this.props.navigation.navigate('Password',{to:'Transfer'})}>
+            <Image
+              source={require('../images/wallet_send.png')}
+              style={{width:18,height:18,marginRight:10}} />
+            <Text>转 账</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.mainAction}
+            onPress={()=>this.props.navigation.navigate('Receipt')}>
+            <Image
+              source={require('../images/wallet_receive.png')}
+              style={{width:18,height:18,marginRight:10}} />
+            <Text>收 款</Text>
+          </TouchableOpacity>
         </View>
-      </View>
-    </TouchableOpacity>
-  )
+      </ImageBackground>
+    )
+  }
+  _renderItem = ({item}) => {
+    const priceObj = this.state.prices[item]
+    const price = priceObj ? priceObj.quote.CNY.price.toFixed(2) : 0.00
+    const status = priceObj ? priceObj.quote.CNY.percent_change_24h.toFixed(2) : 0.00
+    const count = this.state.balances[item] ? this.state.balances[item] : 0
+    const balance = (count * price).toFixed(2)
+    return(
+      <TouchableOpacity onPress={()=>this.props.navigation.navigate('Log')}>
+        <View style={styles.item}>
+          <Text style={{color:'rgb(78,78,78)'}}>{item.toString()}</Text>
+          <View style={{alignItems:'flex-end'}}>
+            <Text style={{fontWeight:'bold',
+              color:status > 0 ? 'blue' : 'red'}}>{price}</Text>
+            <Text style={{fontSize:12,
+              color:status > 0 ? 'blue' : 'red'}}>{status}%</Text>
+          </View>
+          <View style={{alignItems:'flex-end'}}>
+            <Text style={{color:'rgb(46,46,46)'}}>{count}</Text>
+            <Text style={{fontSize:12,color:'rgb(184,186,206)'}}>≈{balance}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
 
   _renderEmpty = () => (
     <View>Empty</View>
@@ -179,12 +223,14 @@ export default class WalletScreen extends Component {
         data={this.state.coins}
         keyExtractor={(item,i) => i.toString()}
         renderItem={this._renderItem}
+        extraData={this.state}
         emptyComponent={this._renderEmpty}
       />
     )
   }
 
   render() {
+    console.log('balances', this.state.balances)
     return (
       <View style={{backgroundColor:'rgb(245,243,251)'}}>
         <ImageBackground
