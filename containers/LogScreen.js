8774702fa550eb027c3379b9e06c3074afa288cc
MacBook <chen@MacBook-Air.local>
2018-10-17 23:20:33 +0800

    update token main page

--- containers/LogScreen.js
+++ containers/LogScreen.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {RefreshControl, ActivityIndicator,Dimensions,ImageBackground, Alert, AsyncStorage,StyleSheet, TouchableOpacity, FlatList, View, Text} from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import colors from '../colors'
-import {formatTime,getCoinsValData} from '../utils'
+import {getBalance, formatTime,getCoinsValData} from '../utils'
 import { deflate } from 'zlib';
 
 const apikey = 'G1T2IX1V1J157RINVS4H1R7QJ3811Z4D6W'
@@ -16,7 +16,9 @@ export default class LogScreen extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      token: null,
       account: null,
+      balance: 0.0000,
       logs: [],
       fetching: true,
       refreshing: false,
@@ -27,6 +29,7 @@ export default class LogScreen extends Component {
   }
 
   componentDidMount() {
+    this.setState({token: this.props.navigation.getParam('token')})
     this._getCoinsValData("ETH")
     this._getAccount()
   }
@@ -49,10 +52,17 @@ export default class LogScreen extends Component {
       .catch((error) => console.log(error))
   }
 
+  // 获取账户
   _getAccount = async() => {
     const account = await AsyncStorage.getItem('account')
     this.setState({account:account})
+    this._getBalance()
     this._fetchLog()
+  }
+
+  // 获取资产
+  _getBalance() {
+    this.setState({balance: getBalance(this.state.account, this.state.token)})
   }
 
   _onRefresh = () => {
@@ -60,9 +70,9 @@ export default class LogScreen extends Component {
     this._fetchLog().then(()=>this.setState({refreshing:false}))
   }
 
+  // 获取交易记录
   _fetchLog = () => {
     let curl = url + `?module=account&action=txlist&address=${this.state.account}&startblock=0&endblock=99999999&sort=asc&apikey=${apikey}`
-    // console.log(curl)
     return fetch(curl)
       .then((response) => response.json())
       .then((responseJson) => {
@@ -101,43 +111,60 @@ export default class LogScreen extends Component {
         imageStyle={{}}
         style={{height:140,lexDirection:'row',justifyContent:'center',}}>
         <View style={{alignSelf:'center'}}>
-          <Text style={{fontSize:28,alignSelf:'center'}}>{this.state.coinNum}</Text>
-          <Text style={{fontSize:16,alignSelf:'center'}}>≈¥{this.state.coinNum*this.state.coinTran}</Text>
+          <Text style={{fontSize:28,alignSelf:'center'}}>
+            {this.state.balance}</Text>
+          <Text style={{fontSize:16,alignSelf:'center'}}>
+            ≈¥{(this.state.balance*this.state.coinTran).toFixed(2)}</Text>
         </View>
       </ImageBackground>
   )
 
+  // 单条记录
   _renderItem = ({item, index}) => (
-    <TouchableOpacity onPress={()=>this.props.navigation.navigate('Detail')} style={{flex:1,backgroundColor:'white',margin:15,borderRadius:3,marginTop:2,marginBottom:8,padding:15,paddingTop:10,paddingBottom:10,flexDirection:'row',justifyContent:'space-between'}}>
+    <TouchableOpacity
+      style={styles.logContainer}
+      onPress={()=>this.props.navigation.navigate('Detail')}>
       <View>
-        <Text style={{color:colors.dark,fontSize:14,lineHeight:20}}>from:{this._filterShort(item.from)}</Text>
-        <Text style={{color:colors.lightgrey,fontSize:12,lineHeight:20}}>{formatTime(item.timeStamp)}</Text>
+        <Text style={styles.logTitle}>
+          from:{this._filterShort(item.from)}</Text>
+        <Text style={styles.logDate}>
+          {formatTime(item.timeStamp)}</Text>
       </View>
       <View>
-        <Text style={{color:colors.dark,fontSize:14,lineHeight:20,color:'#ff9b00'}}>{web3.utils.fromWei(item.value, 'ether')+' ether'}</Text>
-        <Text style={{color:colors.lightgrey,textAlign:'right',fontSize:12,lineHeight:20}}>{this._setState(item.txreceipt_status)}</Text>
-        {/* <Text style={index%2 ? styles.style1: styles.style2}>Hello</Text> */}
+        <Text style={styles.logBalance}>
+          {web3.utils.fromWei(item.value, 'ether')+' ether'}</Text>
+        <Text style={styles.logStatus}>
+          {this._setState(item.txreceipt_status)}</Text>
       </View>
     </TouchableOpacity>
   )
 
+  // 空
   _renderEmpty = () => (
-    <View style={{flex:1,alignItems:'center',justifyContent:'center',marginTop:50}}>
+    <View style={styles.emptyContainer}>
       <Icon name='moon' size={80} color={colors.secondary} />
       <Text style={{color:colors.darkgrey,marginTop:20}}>这里什么也没有</Text>
     </View>
   )
 
+  // 底部按钮
   _renderFooter = () => (
     <View style={{height:46,display:'flex',flexDirection:'row'}}>
-      <TouchableOpacity style={{flex:1,backgroundColor:'#212b66'}} onPress={()=>this.props.navigation.navigate('Transfer')}>
+      <TouchableOpacity
+        style={{flex:1,backgroundColor:'#212b66'}}
+        onPress={()=>this.props.navigation.navigate('Transfer',
+        {token:this.state.token})}>
         <View style={{alignItems:'center',justifyContent:'center',height:42}}>
-            <Text style={{fontSize:14,color:'#ffffff',alignSelf:'center'}}>转  账</Text>
+          <Text style={{fontSize:14,color:'#ffffff',alignSelf:'center'}}>
+            转  账</Text>
         </View>
       </TouchableOpacity>
-      <TouchableOpacity style={{flex:1,backgroundColor:'#27337d'}} onPress={()=>this.props.navigation.navigate('Receipt')}>
+      <TouchableOpacity
+        style={{flex:1,backgroundColor:'#27337d'}}
+        onPress={()=>this.props.navigation.navigate('Receipt')}>
         <View style={{alignItems:'center',justifyContent:'center',height:42}}>
-            <Text style={{fontSize:14,color:'#ffffff',alignSelf:'center'}}>收  款</Text>
+          <Text style={{fontSize:14,color:'#ffffff',alignSelf:'center'}}>
+            收  款</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -172,10 +199,47 @@ export default class LogScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-  style1: {
-    color: 'red',
+  main: {
+
   },
-  style2: {
-    color: 'blue',
+  logContainer: {
+    flex:1,
+    backgroundColor:'white',
+    margin:15,
+    borderRadius:3
+    ,marginTop:2,
+    marginBottom:8,
+    padding:15,
+    paddingTop:10,
+    paddingBottom:10,
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  logTitle: {
+    color:colors.dark,
+    fontSize:14,
+    lineHeight:20
+  },
+  logDate: {
+    color:colors.lightgrey,
+    fontSize:12,
+    lineHeight:20
+  },
+  logBalance: {
+    fontSize:14,
+    lineHeight:20,
+    color:'#ff9b00'
+  },
+  logStatus: {
+    color:colors.lightgrey,
+    textAlign:'right',
+    fontSize:12,
+    lineHeight:20
+  },
+  emptyContainer: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    marginTop:50
   }
 })
