02e42c4c0692ef58511dc9907a2fe637bd973712
xps13 <chen.orange@aliyun.com>
2018-12-22 16:20:55 +0800

    import shim, crypto

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import './global';
+import './shim.js';
+import crypto from 'crypto';
+//import './global';
 import App from './App';
 import {name as appName} from './app.json';
 
