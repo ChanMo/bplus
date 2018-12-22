15eb6b8413b3275fb287dd7dbc01ed35d31c9bdd
xps13 <chen.orange@aliyun.com>
2018-12-22 16:20:29 +0800

    use btoa atob

--- global.js
+++ global.js
@@ -1,17 +1,17 @@
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
 
+if (typeof btoa === 'undefined') {
+  global.btoa = function(str) {
+    return new Buffer(str, "binary").toString("base64");
+  }
+}
+
+if(typeof atob === 'undefined') {
+  global.atob = function(b64Encoded) {
+    return new Buffer(b64Encoded, 'base64').toString('binary');
+  }
+}
+
 const Web3 = require('web3')
 global.web3 = new Web3('http://47.94.206.167:8545');
-
-//if (typeof btoa === 'undefined') {
-//  global.btoa = function(str) {
-//    return new Buffer(str, "binary").toString("base64");
-//  }
-//}
-//
-//if(typeof atob === 'undefined') {
-//  global.atob = function(b64Encoded) {
-//    return new Buffer(b64Encoded, 'base64').toString('binary');
-//  }
-//}
